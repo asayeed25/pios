a54935168e0562837e8e55f50625b7b05de862d9
asayeed25 <azhaansayeed@gmail.com>
2021-01-31 14:40:40 -0600

    bss cleared, working on linked list

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,26 @@
+// function to clear bss
+// source: Lab 1/28/21
 
+#include <stdio.h>
 
+extern long __bss_start;
+extern long __bss_end;
 
 void kernel_main() {
-
+    __bss_zero();
     while(1){
+    }
+}
+void __bss_zero(){
+    char *begin_bss = &__bss_start;
+    char *end_bss = &__bss_end;
+
+//to check if code is functioning as intended
+    begin_bss[0] = 0x0d;
+    
+    int i =0;
+    while(&begin_bss[i] != &end_bss){
+        begin_bss[i] = 0;
+        i++;
     }
 }
---
+++ src/list.c
@@ -0,0 +1,70 @@
+//Linked List
+
+
+ #include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+
+struct list_element {
+	int data;
+	struct list_element* next;
+};
+
+
+int main()
+{
+	struct list_element c = {NULL, 0}; //next plr is NULL, end of list
+	struct list_element b = {&c, 0}; //next plr points to c
+	struct list_element a = {&b, 0}; //next plr points to a 
+	struct list_element *head = &a;
+	
+	return 0;
+
+}
+
+/*
+//https://www.geeksforgeeks.org/linked-list-set-1-introduction/
+
+struct Node { 
+    int data; 
+    struct Node* next; 
+}; 
+  
+// This function prints contents of linked list starting from 
+// the given node 
+void printList(struct Node* n) 
+{ 
+    while (n != NULL) { 
+        printf(" %d ", n->data); 
+        n = n->next; 
+    } 
+} 
+  
+int main() 
+{ 
+    struct Node* head = NULL; 
+    struct Node* second = NULL; 
+    struct Node* third = NULL; 
+  
+    // allocate 3 nodes in the heap 
+    head = (struct Node*)malloc(sizeof(struct Node)); 
+    second = (struct Node*)malloc(sizeof(struct Node)); 
+    third = (struct Node*)malloc(sizeof(struct Node)); 
+  
+    head->data = 1; // assign data in first node 
+    head->next = second; // Link first node with second 
+  
+    second->data = 2; // assign data to second node 
+    second->next = third; 
+  
+    third->data = 3; // assign data to third node 
+    third->next = NULL; 
+  
+    printList(head); 
+  
+    return 0; 
+}
+
+*/
+
